7a2503d56c8c0c0b7ba07eecfca5c14e94c133ae
Jennifer Tran <jennantran@gmail.com>
2020-06-08 12:01:00 -0700

    Updates

---
+++ precache-manifest.3825a4ee37a3b5a50888d0002dfc6b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1ac56bbde61ba3866a14f251f3827b",
+    "url": "/letstourit-app/index.html"
+  },
+  {
+    "revision": "e3358930389d94f50e04",
+    "url": "/letstourit-app/static/css/main.b3178ae5.chunk.css"
+  },
+  {
+    "revision": "ca8a192616bc63143ddb",
+    "url": "/letstourit-app/static/js/2.7c87a185.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/letstourit-app/static/js/2.7c87a185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3358930389d94f50e04",
+    "url": "/letstourit-app/static/js/main.ed150d65.chunk.js"
+  },
+  {
+    "revision": "53e295d98733f406ec80",
+    "url": "/letstourit-app/static/js/runtime-main.828250bd.js"
+  },
+  {
+    "revision": "a050b103cc2012c02023f0a383f4acd5",
+    "url": "/letstourit-app/static/media/compass.a050b103.jpg"
+  }
+]);
