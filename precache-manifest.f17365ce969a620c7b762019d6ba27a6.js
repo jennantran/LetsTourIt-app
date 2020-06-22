efd4483a3acef7c89e3e5fa134ff47cf94a9696a
Jennifer Tran <jennantran@gmail.com>
2020-06-21 20:46:55 -0700

    Updates

---
+++ precache-manifest.f17365ce969a620c7b762019d6ba27a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711c7285c2834adf562135119cd7420c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e6506073ef6f3dab052",
+    "url": "/static/css/main.37c38f8b.chunk.css"
+  },
+  {
+    "revision": "4edc85725f5d5aa479fe",
+    "url": "/static/js/2.c3ed10dc.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.c3ed10dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6506073ef6f3dab052",
+    "url": "/static/js/main.c12cd31b.chunk.js"
+  },
+  {
+    "revision": "f0e419f0207169fae08f",
+    "url": "/static/js/runtime-main.b8b8ca80.js"
+  },
+  {
+    "revision": "a050b103cc2012c02023f0a383f4acd5",
+    "url": "/static/media/compass.a050b103.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/letstourit-app/precache-manifest.94744ee86ff1ab10ae46dca53a151ba1.js"
+  "/precache-manifest.f17365ce969a620c7b762019d6ba27a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/letstourit-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
