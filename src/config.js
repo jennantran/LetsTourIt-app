3282c8bd219269c0b22c1ed4481bd15788aaaa7d
Jennifer Tran <jennantran@gmail.com>
2020-06-22 18:03:57 -0700

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://young-citadel-05531.herokuapp.com/api`,
+    API_ENDPOINT: `https://young-citadel-05531.herokuapp.com`,
     TOKEN_KEY: 'tour-client-auth-token',    
   }
