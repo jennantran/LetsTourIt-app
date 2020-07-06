5b3114451dfc3feead75761b8ba0aec8c14aa0b4
Jennifer Tran <jennantran@gmail.com>
2020-07-06 16:22:23 -0700

    responsive homepage styling

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -16,7 +16,7 @@ import './HomePage.css';
                     </div>
                     <div className='header'>
                         <h1 className='appTitle'>Let the Exploring Begin</h1>
-                        <h2>Search for things to do and things to eat.</h2>
+                        <h2 className='motto'>Search for things to do and things to eat.</h2>
                         <p className='homepageDetails'>
                         Sign up and create an account to generate a favorited list!
                         Search up places based on your location and click on the heart icon to add to your list.
@@ -24,20 +24,6 @@ import './HomePage.css';
                         <button onClick={e => this.handleClick(e)}>Sign Up</button>
                     </div>
                 </section>
-                {/* <section className='details'>
-                        <div id='col1'>
-                            <i class="fas fa-user-plus"></i>
-                            <p>Sign up and create an account to generate a favorited list!</p>
-                        </div>
-                        <div id='col2'>
-                            <i class="fas fa-search"></i>
-                            <p>Search up places based on your location and click on the heart icon to add to your list.</p>
-                        </div>
-                        <div id='col3'>
-                            <i class="fas fa-list-ul"></i>
-                            <p>Refer back to the list whenever you log in to your account.</p>
-                        </div>
-                </section> */}
             </div>
         );
     }
